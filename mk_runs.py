51ebebae7cd2b1fd7db4789eb1b3379a56c228e1
Peter Teuben <teuben@gmail.com>
2023-05-04 21:12:51 -0400

    typo

--- mk_runs.py
+++ mk_runs.py
@@ -1,16 +1,12 @@
 #! /usr/bin/env python
 #
-#   script generator for project="2021-S1-US-3"
-#
-#   lmtinfo.py grep US-3 Science Map | awk '{print $2}' | sort
-
 
 import os
 import sys
 
 from lmtoy import runs
 
-project="2023-S1-2023S1SEQUOIACommissioning"
+project="2023S1SEQUOIACommissioning"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
@@ -26,8 +22,8 @@ on['chi-Cyg'] = [-107662, 107664, 107665, 107666, 107667 ]      # 2-apr
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 pars1['chi-Cyg']   = "pix_list=-3,-13 extent=120"
-pars1["M17SW"] = ""
-pars1["VX-Sgr"] = ""
+pars1["M17SW"]     = "dv=50 dw=50"
+pars1["VX-Sgr"]    = ""
 
 
 #        common parameters per source on subsequent runs (run1b, run2b)

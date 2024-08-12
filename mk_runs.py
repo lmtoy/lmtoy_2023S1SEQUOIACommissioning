947d4454cd27a5951d1bdff298a7cbeaf38e1fbe
Peter Teuben <teuben@gmail.com>
2024-08-12 11:45:21 -0400

    upd

--- mk_runs.py
+++ mk_runs.py
@@ -28,18 +28,25 @@ pars1 = {}
 pars1['chi-Cyg']   = "pix_list=-3,-13 extent=120"          # -3 is needed, or it will crash, 13 just high Tsys
 pars1["M17SW"]     = "dv=50 dw=50"
 pars1["VX-Sgr"]    = ""
-pars1["R-Leo"]     = "dv=50 dw=50 extent=240"
+#pars1["R-Leo"]     = "dv=50 dw=50 extent=240"
+pars1["R-Leo"]     = "extent=240"
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
 pars2['chi-Cyg']   = "srdp=1 admit=0"
 pars2["M17SW"]     = ""
 pars2["VX-Sgr"]    = ""
-pars2["R-Leo"]     = "pix_list=-5,12,13"
+pars2["R-Leo"]     = "bank=0 pix_list=-5,12,13"
 
 #pars2['M17SW']  = ["bank=0 pix_list=-0,5,12,13,14,15",
 #	  	    "bank=1 pix_list=-0,2,3,4,12,13"]
 
+pars3 = {}
+pars2['chi-Cyg']   = ""
+pars2["M17SW"]     = ""
+pars2["VX-Sgr"]    = ""
+pars2["R-Leo"]     = "bank=1 pix_list=-0,4,8,9"   # no line
+
 
 if __name__ == '__main__':    
-    runs.mk_runs(project, on, pars1, pars2, sys.argv)
+    runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)

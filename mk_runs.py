70c48e4682d6dfec600ab3cf802d84a81cd634bc
Peter Teuben <teuben@gmail.com>
2024-10-03 15:49:54 +0000

    upd

--- mk_runs.py
+++ mk_runs.py
@@ -23,6 +23,10 @@ on['chi-Cyg'] = [-107662, 107664, 107665, 107666, 107667,                       
                  108781, 108782, 108783, 108787, ]
 #RT-Vir  109958 109963
 
+on['KZ-Peg']  = [ 107996 ]                                                      # only 1 obs
+
+on['MonR2']  = [ 110405, 110407, 110411, ]
+
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 pars1['chi-Cyg']   = "pix_list=-3,-13 extent=120"          # -3 is needed, or it will crash, 13 just high Tsys
@@ -30,6 +34,10 @@ pars1["M17SW"]     = "dv=50 dw=50"
 pars1["VX-Sgr"]    = ""
 #pars1["R-Leo"]     = "dv=50 dw=50 extent=240"
 pars1["R-Leo"]     = "extent=240"
+pars1["KZ-Peg"]    = "extent=120"
+
+pars1['MonR2']  = "extent=500"
+
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
@@ -37,16 +45,19 @@ pars2['chi-Cyg']   = "srdp=1 admit=0"
 pars2["M17SW"]     = ""
 pars2["VX-Sgr"]    = ""
 pars2["R-Leo"]     = "bank=0 pix_list=-5,12,13"
+pars2["KZ-Peg"]    = "bank=0 pix_list=-14,15"
+pars2['MonR2']    = "bank=0 pix_list=-11,13"
 
 #pars2['M17SW']  = ["bank=0 pix_list=-0,5,12,13,14,15",
 #	  	    "bank=1 pix_list=-0,2,3,4,12,13"]
 
 pars3 = {}
-pars2['chi-Cyg']   = ""
-pars2["M17SW"]     = ""
-pars2["VX-Sgr"]    = ""
-pars2["R-Leo"]     = "bank=1 pix_list=-0,4,8,9"   # no line
-
+pars3['chi-Cyg']   = ""
+pars3["M17SW"]     = ""
+pars3["VX-Sgr"]    = ""
+pars3["R-Leo"]     = "bank=1 pix_list=-0,4,8,9"   # no line
+pars3["KZ-Peg"]    = "bank=1 pix_list=-0"
+pars2['MonR2']    = "bank=1"
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
